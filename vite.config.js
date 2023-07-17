1a750bfcb11325b602f9a7f17e79fe87486638b2
rcrosbourne <rcrosbourne@gmail.com>
2023-07-17 10:28:14 -0500

    Install breeze

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,14 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: 'resources/js/app.tsx',
+            ssr: 'resources/js/ssr.tsx',
             refresh: true,
         }),
+        react(),
     ],
-    server: {
-        hmr: {
-            protocol: 'wss',
-            host: 'boj-exchange.ddev.site',
-        },
-        host: '0.0.0.0',
-        strictPort: true,
-        // dev server port
-        port: 3000,
-        // SSR server port
-        // port: 13714,
-    },
 });
