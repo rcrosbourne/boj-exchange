fa09563e7ed2cdb4a965f731b4f5d2cb81200ab5
rcrosbourne <rcrosbourne@gmail.com>
2023-07-17 10:19:13 -0500

    Dev setup complete

--- vite.config.js
+++ vite.config.js
@@ -8,4 +8,16 @@ export default defineConfig({
             refresh: true,
         }),
     ],
+    server: {
+        hmr: {
+            protocol: 'wss',
+            host: 'boj-exchange.ddev.site',
+        },
+        host: '0.0.0.0',
+        strictPort: true,
+        // dev server port
+        port: 3000,
+        // SSR server port
+        // port: 13714,
+    },
 });
