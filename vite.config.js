9c54f513953ffd304c689a2b8f8e72605c256b1b
rcrosbourne <rcrosbourne@gmail.com>
2023-07-23 07:05:44 -0500

    testing deployment

--- vite.config.js
+++ vite.config.js
@@ -11,4 +11,16 @@ export default defineConfig({
         }),
         react(),
     ],
+    server: {
+        hmr: {
+            protocol: 'wss',
+            host: 'boj-exchange.ddev.site',
+        },
+        host: '0.0.0.0',
+        strictPort: true,
+        // dev server port
+        port: 3000,
+        // SSR server port
+        // port: 13714,
+    },
 });
