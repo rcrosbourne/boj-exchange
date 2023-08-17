79473853ed0079962d57edd340bbdb82060a9432
rcrosbourne <rcrosbourne@gmail.com>
2023-08-17 13:32:11 -0400

    feat: add index to exchange table

---
+++ database/migrations/2023_08_17_172904_add-index-to-exchange-table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('exchange_rates', function (Blueprint $table) {
+            $table->index(['currency', 'sell_price', 'date'], 'currency_sell_price_date_index');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('exchange_rates', function (Blueprint $table) {
+            $table->dropIndex('currency_sell_price_date_index');
+        });
+    }
+};
