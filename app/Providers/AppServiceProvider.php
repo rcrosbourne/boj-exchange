040ef6229a01a047dd04d24cffecd17d6979d598
rcrosbourne <rcrosbourne@gmail.com>
2023-10-16 05:29:51 -0500

    force https in production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,6 +21,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        //force https in production
+        if ($this->app->environment('production')) {
+            \URL::forceScheme('https');
+        }
     }
 }
